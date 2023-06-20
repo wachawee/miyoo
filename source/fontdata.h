1e7a21ad3f40d928100cee4690284f6e9685c2f1
wachawee <46044664+wachawee@users.noreply.github.com>
2023-06-20 21:16:22 +0700

    Delete source directory

--- source/fontdata.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-std::string get_font_data();
-
-extern uint8_t defaultFontBinaryData[2048];
--- source/mathhelpers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//std::clamp is in c++17, but not c++11
-int clamp (int val, int lo, int hi);
-
-float clamp (float val, float lo, float hi);
-
-float lerp (float a, float b, float t);
-
